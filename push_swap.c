8e52527e9d7dcdbd28fe1850b18991d171b23880
QuentinRabe <lovegoodandyh@gmail.com>
2024-05-24 19:59:38 +0300

    feat(push_swap.c): add creation stack_a function
    Added 'create_stack_a' function from 'stack.c' in the main program of
    the project.

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: quentin <quentin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 11:49:43 by quentin           #+#    #+#             */
-/*   Updated: 2024/05/24 12:08:14 by quentin          ###   ########.fr       */
+/*   Updated: 2024/05/24 19:53:40 by quentin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 int	main(int ac, char **av)
 {
-	check_args(ac, av);
+	char		**inputs;
+	t_stack		*stack_a;
+
+	inputs = check_args(ac, av);
+	stack_a = NULL;
+	create_stack_a(&stack_a, inputs);
 	return (0);
 }
