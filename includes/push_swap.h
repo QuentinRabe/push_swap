dc333abc3e0855d2a25cb41595c1fb8e6c7494e6
QuentinRabe <lovegoodandyh@gmail.com>
2024-05-24 20:06:12 +0300

    refactor(push_swap.h): update functions prototypes
    Added stack_a manipulation functions prototypes (creating and freeing).

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: quentin <quentin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 11:49:54 by quentin           #+#    #+#             */
-/*   Updated: 2024/05/24 18:50:02 by quentin          ###   ########.fr       */
+/*   Updated: 2024/05/24 19:58:51 by quentin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,18 @@
 # include "../ft_printf/ft_printf.h"
 # include <limits.h>
 
-typedef struct s_stack_a
+typedef struct s_stack
 {
-	struct s_stack_a	*prev;
-	struct s_stack_a	*next;
-	int					num;
-}	t_stack_a;
+	struct s_stack	*prev;
+	struct s_stack	*next;
+	int				num;
+}	t_stack;
 
 /*-------------ERRORS-------------*/
 char	**check_args(int ac, char **av);
 void	free_array(char ***array);
 void	check_if_valide_input(char *str);
+/*-------------STACK_A-------------*/
+void	create_stack_a(t_stack **head, char **array);
+void	free_list(t_stack **head);
 #endif
