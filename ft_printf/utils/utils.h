c5917828a27ce72384663d563eaa7e188610c250
QuentinRabe <lovegoodandyh@gmail.com>
2024-05-24 12:55:46 +0300

    fix(ft_printf): remove 'ft_atoi' prototype
    Removed 'ft_atoi' prototype in 'utils.h' header file that was useless.

--- ft_printf/utils/utils.h
+++ ft_printf/utils/utils.h
@@ -6,7 +6,7 @@
 /*   By: quentin <quentin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 08:28:38 by arabefam          #+#    #+#             */
-/*   Updated: 2024/05/24 11:19:14 by quentin          ###   ########.fr       */
+/*   Updated: 2024/05/24 12:53:52 by quentin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@
 // ===============INT===============
 int		ft_isdigit(int c);
 int		ft_count_digit(int n);
-int		ft_atoi(const char *nptr);
 int		ft_putnbr_base(unsigned long nbr, char *base);
 // ===============VOID===============
 void	ft_putnbr_fd(int n, int fd);
