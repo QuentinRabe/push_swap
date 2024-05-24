5a5f4806668343f81f0559e51335d7bf8351ed23
QuentinRabe <lovegoodandyh@gmail.com>
2024-05-24 16:22:09 +0300

    feat(includes): add 'push_swap.h' header file
    Added 'push_swap.h' header file to the 'includes' directory that contain
    all the prototypes of functions that I will use in the project.

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,14 +6,19 @@
 /*   By: quentin <quentin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 11:49:54 by quentin           #+#    #+#             */
-/*   Updated: 2024/05/24 11:51:23 by quentin          ###   ########.fr       */
+/*   Updated: 2024/05/24 16:04:20 by quentin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include "./libft/libft.h"
-# include "./ft_printf/ft_printf.h"
+# include "../libft/libft.h"
+# include "../ft_printf/ft_printf.h"
+# include <limits.h>
 
+/*-------------ERRORS-------------*/
+void	check_args(int ac, char **av);
+void	free_array(char ***array);
+void	check_if_valide_input(char *str);
 #endif
