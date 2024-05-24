b70886cd6f2a7ef479c6cb053d5d3e1e740f9fdb
QuentinRabe <lovegoodandyh@gmail.com>
2024-05-24 20:04:45 +0300

    feat(free_list): add free function for lists
    Added a function 'free_list' to free double linked lists structure.

--- utils/free_helper.c
+++ utils/free_helper.c
@@ -6,11 +6,26 @@
 /*   By: quentin <quentin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 13:02:13 by quentin           #+#    #+#             */
-/*   Updated: 2024/05/24 13:03:35 by quentin          ###   ########.fr       */
+/*   Updated: 2024/05/24 19:51:19 by quentin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+void	free_list(t_stack **head)
+{
+	t_stack	*current;
+
+	current = NULL;
+	while (*head)
+	{
+		current = (*head)->next;
+		ft_printf("Freeing node with num-> %d.\n", (*head)->num);
+		free(*head);
+		*head = current;
+	}
+	*head = NULL;
+}
 
 void	free_array(char ***array)
 {
