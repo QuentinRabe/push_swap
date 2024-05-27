0918fbf94f36d427d039e8b968728e3b378e86d1
QuentinRabe <lovegoodandyh@gmail.com>
2024-05-27 10:51:40 +0300

    feat(init_a): add function to init stack_a
    Added 'init_stack_a' function to store all inputs in double linked list.

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: arabefam <arabefam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 11:49:43 by quentin           #+#    #+#             */
-/*   Updated: 2024/05/27 08:52:07 by arabefam         ###   ########.fr       */
+/*   Updated: 2024/05/27 10:48:43 by arabefam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,15 @@
 int	main(int ac, char **av)
 {
 	char		**inputs;
-	t_stack		*stack_a;
+	t_stack		*a;
+	t_stack		*b;
 
+	a = NULL;
+	b = NULL;
+	(void) b;
 	inputs = check_args(ac, av);
-	stack_a = NULL;
+	init_stack_a(&a, inputs);
+	free_array(&inputs);
+	free_list(&a);
 	return (0);
 }
---
+++ utils/init_a.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_a.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arabefam <arabefam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/27 08:55:29 by arabefam          #+#    #+#             */
+/*   Updated: 2024/05/27 09:18:10 by arabefam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+t_stack	*get_last_node(t_stack *first)
+{
+	t_stack	*last;
+
+	last = first;
+	while (last->next)
+		last = last->next;
+	return (last);
+}
+
+void	append_node(t_stack	*a, char *number, char **array)
+{
+	t_stack	*last;
+	t_stack	*new;
+
+	new = NULL;
+	new = (t_stack *) malloc(sizeof(t_stack));
+	if (!new)
+	{
+		free_array(&array);
+		free_list(&a);
+		return ;
+	}
+	last = get_last_node(a);
+	new->prev = last;
+	new->next = NULL;
+	new->num = ft_atoi(number);
+	last->next = new;
+}
+
+void	init_stack_a(t_stack **a, char **array)
+{
+	t_stack	*new;
+	int		i;
+
+	new = (t_stack *) malloc(sizeof(t_stack));
+	if (!new)
+		return (free_array(&array));
+	new->num = ft_atoi(array[0]);
+	new->next = NULL;
+	new->prev = NULL;
+	*a = new;
+	i = 0;
+	while (array[++i])
+		append_node(*a, array[i], array);
+}
