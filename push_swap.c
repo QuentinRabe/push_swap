b59d8ed8d77375d56d4f49082df7a0f19d012fe5
QuentinRabe <lovegoodandyh@gmail.com>
2024-05-28 10:16:37 +0300

    feat(print_stacks): add function to print stack
    Added 'print_stack' function to visualize instructions behavior.
    The code will be easier to debug then.

--- push_swap.c
+++ push_swap.c
@@ -3,14 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arabefam <arabefam@student.42.fr>          +#+  +:+       +#+        */
+/*   By: quentin <quentin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 11:49:43 by quentin           #+#    #+#             */
-/*   Updated: 2024/05/27 10:48:43 by arabefam         ###   ########.fr       */
+/*   Updated: 2024/05/28 10:14:36 by quentin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/push_swap.h"
+
+void	print_stacks(t_stack *a, t_stack *b)
+{
+	t_stack	*head_a;
+	t_stack	*head_b;
+
+	head_a = a;
+	head_b = b;
+	if (head_a)
+	{
+		while (head_a)
+		{
+			ft_printf("[ %d ]\t", head_a->num);
+			head_a = head_a->next;
+		}
+		ft_printf("%d nodes\n", count_node(a));
+	}
+	ft_putchar_fd('\n', 1);
+	if (head_b)
+	{
+		while (head_b)
+		{
+			ft_printf("[ %d ]\t", head_b->num);
+			head_b = head_b->next;
+		}
+		ft_printf("%d nodes\n", count_node(b));
+	}
+	ft_putchar_fd('\n', 1);
+}
 
 int	main(int ac, char **av)
 {
@@ -20,10 +49,10 @@ int	main(int ac, char **av)
 
 	a = NULL;
 	b = NULL;
-	(void) b;
 	inputs = check_args(ac, av);
 	init_stack_a(&a, inputs);
 	free_array(&inputs);
 	free_list(&a);
+	free_list(&b);
 	return (0);
 }
